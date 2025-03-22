465bcfd461b8e2d681d3af64e4450dd0806da20a
ThomasSemenak <157653726+ThomasSemenak@users.noreply.github.com>
2025-03-21 23:44:44 -0400

    Add files via upload

---
+++ test_connection.php
@@ -0,0 +1,20 @@
+<?php
+// Azure SQL Database connection details
+$serverName = "mysqlserverjadentest.database.windows.net";
+$connectionOptions = array(
+    "Database" => "mySampleDatabase",
+    "Uid" => "azureuser",
+    "PWD" => "password@1",
+    "TrustServerCertificate" => true
+);
+
+// Establish the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+// Check connection and output result
+if ($conn) {
+    echo "yes";
+} else {
+    echo "no";
+}
+?>
