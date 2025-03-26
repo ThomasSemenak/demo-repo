cabc152dd3cf4a9a2d89652b26dfba71be0ca679
ThomasSemenak <157653726+ThomasSemenak@users.noreply.github.com>
2025-03-26 15:03:03 -0400

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    session_unset(); //null all session variables
-    session_destroy(); //destroy session
-    header("src/Login/login_page.php"); //redirect to login page
-    exit();
-?>
