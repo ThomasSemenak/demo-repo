d32149c50a4909211c47e7f82c92268fba273264
ThomasSemenak <157653726+ThomasSemenak@users.noreply.github.com>
2025-03-29 20:03:39 -0400

    Update header.php

--- views/header.php
+++ views/header.php
@@ -33,8 +33,8 @@ function isActive($page) {
     <nav id="main-nav-bar">
         <a href="<?php echo $base_url;?>index.php" class="<?php echo isActive('index.php');?>">Dashboard</a>
         <a href="<?php echo $base_url;?>src/Generate/generate_page.php" class="<?php echo isActive('generate_page.php');?>">Generate</a>
-        <a href="<?php echo $base_url;?>index.php" class="<?php echo isActive('about_us.php');?>">About Us</a>
-        <a href="<?php echo $base_url;?>index.php" class="<?php echo isActive('faq.php');?>">FAQ</a>
+        <a href="<?php echo $base_url;?>about_us.php" class="<?php echo isActive('about_us.php');?>">About Us</a>
+        <a href="<?php echo $base_url;?>faq.php" class="<?php echo isActive('faq.php');?>">FAQ</a>
         
         <?php if (isset($_SESSION['user_id'])): ?>  
             <div class="profile-dropdown">
