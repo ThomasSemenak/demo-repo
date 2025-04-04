44c0ad2a1fa524a7682313ec763d5b13605c2092
ThomasSemenak <157653726+ThomasSemenak@users.noreply.github.com>
2025-04-03 23:30:18 -0400

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,13 +2,21 @@
 <html>
 <head>
 	<title>User Registration | PHP</title>
+    <link rel="stylesheet" type"text/css" href="css/boostrap.min.css">
 </head>
 <body>
 
 <div>
 	<?php
 	if(isset($_POST['create'])){
-        echo 'User submitted';
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $phonenumber = $_POST['phonenumber'];
+        $password = $_POST['password'];
+
+        
+        echo $firstname. " "  .$lastname ." ". $email ."" .$phonenumber."" .$password ;
     }
 	?>	
 </div>
@@ -16,26 +24,34 @@
 <div>
 	<form action="registration.php" method="post">
 		<div class="container">
-            <h1>Registration</h1>
-            <p>Fill up the form with correct values.</p>
+            <div class="row">
+                <div class="col-sm-3">
+                    <h1>Registration</h1>
+                    <p>Fill up the form with correct values.</p>
+                    <hr class="mb-3">
 
-            <label for="firstname"><b>First Name</b></label>
-            <input type="text" name="firstname" required>
 
-            <label for="lastname"><b>Last Name</b></label>
-            <input type="text" name="lastname" required>
 
-            <label for="email"><b>Email Address</b></label>
-            <input type="email" name="email" required>
+                    <label for="firstname"><b>First Name</b></label>
+                    <input class="form-control" type="text" name="firstname" required>
 
-            <label for="phonenumber"><b>Phone Number</b></label>
-            <input type="text" name="phonenumber" required>
+                    <label for="lastname"><b>Last Name</b></label>
+                    <input class="form-control" type="text" name="lastname" required>
 
-            <label for="password"><b>Password</b></label>
-            <input type="password" name="password" required>
-            
-            <input type="submit" name="create" value="Sign Up">
-				
+                    <label for="email"><b>Email Address</b></label>
+                    <input class="form-control" type="email" name="email" required>
+
+                    <label for="phonenumber"><b>Phone Number</b></label>
+                    <input class="form-control" type="text" name="phonenumber" required>
+
+                    <label for="password"><b>Password</b></label>
+                    <input class="form-control" type="password" name="password" required>
+
+                    <hr class="mb-3">
+        
+                    <input class="btn btn-primary" type="submit" name="create" value="Sign Up">
+			    </div>	
+            </div>
 		</div>
 	</form>
 </div>
