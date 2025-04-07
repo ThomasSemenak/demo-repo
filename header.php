a486375f42f4c858576645fbfb813c94bae30063
ThomasSemenak <157653726+ThomasSemenak@users.noreply.github.com>
2025-04-07 18:30:03 -0400

    Update header.php

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
      <div>
          <a href="welcome.php" style="margin-right: 15px; text-decoration: none;">Welcome</a>
          <a href="index.php" style="margin-right: 15px; text-decoration: none;">Dashboard</a>
-         <a href="generate.php" style="margin-right: 15px; text-decoration: none;">Generate</a>
+         <a href="/generate/generate_page.php" style="margin-right: 15px; text-decoration: none;">Generate</a>
          <a href="aboutus.php" style="margin-right: 15px; text-decoration: none;">About Us</a>
          <a href="faq.php" style="text-decoration: none;">FAQ</a>
      </div>
